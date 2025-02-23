50f148a35e511a4c394ec1c0cd8f6a5812e83b0f
RogerEscobar <dg.rogerescobar@gmail.com>
2025-02-23 07:12:57 -0500

    Frase de inicio

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-5xl font-bold text-blue-600">
-        Portafolio acá vamos🚀
+        Hola, soy Roger Escobar Diseño experiencias digitales centradas en el
+        usuario, integrando UX/UI, Motion y Código.
       </h1>
+      <h2>Estoy construyendo mi portafolio</h2>
     </div>
   );
 }
